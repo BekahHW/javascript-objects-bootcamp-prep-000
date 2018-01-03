eaeebd7156b7ae4598bd00ac2901dd7e3b8ca54c
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-03 18:55:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,7 +8,11 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 
 function removeFromPlaylist(playlist, artistName){
+<<<<<<< HEAD
 delete playlist[artistName]
 return playlist
+=======
+delete playlist.artistName
+>>>>>>> 100a8fff60ad2cec78d2e4c2d7e7deda1d3bf888
 
 }
